3a6629addbd8bf7429c60113adfd0a6a4dffa95f
40-02 <uvaroff@40-02Air.local>
2015-08-23 21:33:09 +0300

    making it prettier

--- getDataFromMetrikaFunction.m
+++ getDataFromMetrikaFunction.m
@@ -1,22 +1,25 @@
-// Функция, при помощи которой мы забираем из API данные из Яндекс.Метрики
-// Версия 8.1
-
+/*
+    // Функция, при помощи которой мы забираем из API данные из Яндекс.Метрики
+    // Версия 1.02
+    // 
+    // Справка по api метрики:
+    // https://tech.yandex.ru/metrika/doc/api2/concept/about-docpage/
+*/
 
 let
     metrikaFunction = (ids as text, dimensions as nullable text, metrics as text, date1 as text, date2 as text, token as text, optional filters as text) => 
 
 
-// Определяем функцию metrika_fun внутри функции, которая будет вытаскивать CSV из API. 
-// На входе конфигурационная запись (формируется в основном теле программы ниже по коду).
+        // Определяем функцию metrika_fun внутри функции, которая будет вытаскивать CSV из API. 
+        // На входе конфигурационная запись (формируется в основном теле программы ниже по коду).
 
 let
-
     metrika_fun = (bigRecordWithOptions as record, X as number) =>
 let
     offset = Number.ToText (X),
     bigRecordWithWithOffset = Record.AddField(bigRecordWithOptions, "offset", offset),
     bigRecordWithWithLimit =  Record.AddField(bigRecordWithWithOffset , "limit", "10000"),
-    urlToGet = "https://api-metrika.yandex.ru/stat/v1/data.csv?" & Uri.BuildQueryString (bigRecordWithWithOffset),
+    urlToGet = "https://api-metrika.yandex.ru/stat/v1/data.csv?" & Uri.BuildQueryString (bigRecordWithWithLimit),
 
     Source = Csv.Document(Web.Contents(urlToGet),null,",",null,65001),
     #"First Row as Header" = Table.PromoteHeaders(Source),
@@ -25,7 +28,7 @@ let
 in
     mergedColumns,
 
-// Определяем функцию metrika_json внутри функции, которая будет выгружать json версию отчета и возвращать из json максимальное количество строчек
+        // Определяем функцию metrika_json внутри функции, которая будет выгружать json версию отчета и возвращать из json максимальное количество строчек
     metrika_json = (bigRecordWithOptions as record) =>
 let
     urlToGet = "https://api-metrika.yandex.ru/stat/v1/data?" & Uri.BuildQueryString (bigRecordWithOptions),
@@ -35,24 +38,23 @@ let
 in
     Source7,
 
-// Основной код функции начинается здесь.
-
-// Формируем конфигурационную запись (record) для использования в функциях
+        // Основной код функции начинается здесь.
+        // Формируем конфигурационную запись (record) для использования в функциях
     bigRecordWithOptions = [ids = ids, dimensions = dimensions, metrics = metrics, date1 = date1, date2 = date2, token = token, accuracy = "full"], 
     bigRecordWithFilters = if filters = null then bigRecordWithOptions else Record.AddField(bigRecordWithOptions, "filters", filters), 
 
-// Создаем список из чисел - сколько раз нам необходимо обратиться к api чтобы забрать по 10к строчек все данныые которые есть в метрке согласно нашим настройкам. 
+        // Создаем список из чисел - сколько раз нам необходимо обратиться к api чтобы забрать по 10к строчек все данныые которые есть в метрке согласно нашим настройкам. 
     Source0 = Number.RoundDown(metrika_json(bigRecordWithFilters)/10000,0),
     Custom1 = {0..Source0},
     Custom2 = List.Transform(Custom1, each _ * 10000 + 1),
 
-// Используем список получившихся чисел, чтобы обратиться к api
+        // Используем список получившихся чисел, чтобы обратиться к api
     Custom3 = List.Transform(Custom2, each metrika_fun(bigRecordWithFilters, _)),
 
-// Создаем список с названиями заголовков полей
+        // Создаем список с названиями заголовков полей
     TableNames = Table.ColumnNames(Custom3{0}),
 
-// Разворачиваем таблицы в списке с данными из Яндекс.Метрики и используем список с названиям заголовков, которые мы получили ранее
+        // Разворачиваем таблицы в списке с данными из Яндекс.Метрики и используем список с названиям заголовков, которые мы получили ранее
     #"Converted to Table" = Table.FromList(Custom3, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
     #"Expanded Column1" = Table.ExpandTableColumn(#"Converted to Table", "Column1", TableNames )
 
--- readme.md
+++ readme.md
@@ -1,5 +1,19 @@
-## Экспорт данных из Яндекс.Метрики
+# Экспорт данных из Яндекс.Метрики #
+
+Программа для экспорта данных из api Яндекс.Метрики в MS Excel, с использованием надстройки [MS Power Query](https://www.microsoft.com/en-us/download/details.aspx?id=39379&WT.mc_id=Blog_PBI_Announce_DI).
+
+## Начало работы ##
+
+1. [Скачиваем последнюю версию Power Query](https://www.microsoft.com/en-us/download/details.aspx?id=39379&WT.mc_id=Blog_PBI_Announce_DI) и устанавливаем ее
+2. [Скачиваем последнюю версию файла PQMetrika.xlsm](https://github.com/40-02/PQYandexMetrika/releases/latest)
+3. Открываем файл
+4. Включаем галку [ignore the privacy levels](http://dl.getdropbox.com/u/390630/2015-08-23%2021_29_16.gif)
 
 ## Полезные ссылки
 
-[marketing wiki](http://marketing-wiki.ru/wiki/Экспорт_данных_из_сервиса_Яндекс.Метрика_в_excel_(power_query))
+1. [Статья в marketing-wiki](http://marketing-wiki.ru/wiki/Экспорт_данных_из_сервиса_Яндекс.Метрика_в_excel_(power_query))
+2. [MS Power Query](https://www.microsoft.com/en-us/download/details.aspx?id=39379&WT.mc_id=Blog_PBI_Announce_DI)
+
+
+![ignore the privacy levels](http://dl.getdropbox.com/u/390630/2015-08-23%2021_29_16.gif)
+
